3618960d49559f3af23a4c2a89d6fa7633f393a9
rob-lambeth <rlambeth3@gmail.com>
2021-11-24 16:49:18 -0500

    RL quick fix
    Somehow top chunk of my code was removed, which caused errors for whole doc.

--- script.js
+++ script.js
@@ -24,48 +24,50 @@ const projection = d3
 const path = d3.geoPath().projection(projection)
 
 //create tooltip
-const tooltip = d3.select("#chart").append("div")
-        .attr("class", "tooltip")
-        .style("opacity", 0);
-    
-d3.csv("bystate_fromcz_rounded.csv", function(data) {
-    //grab the variable theil index - used to measure economic inequality
-    var theil = 'cs_race_theil_2000';
+const tooltip = d3
+  .select("#chart")
+  .append("div")
+  .attr("class", "tooltip")
+  .style("opacity", 0)
 
-    //set colors - the darker the red, the more inequality there are
-    lowColor = '#ffffff';
-    highColor = '#c43333';
+d3.csv("bystate_fromcz_rounded.csv", function (data) {
+  //grab the variable theil index - used to measure economic inequality
+  var theil = "cs_race_theil_2000"
 
-    //get grad rate min and max
-    let max = d3.max(data, function (d, i) {
-        return d[theil];
-    });
-    let min = d3.min(data, function (d, i) {
-        return d[theil];
-    });
+  //set colors - the darker the red, the more inequality there are
+  lowColor = "#ffffff"
+  highColor = "#c43333"
 
-    //color ramp
-    var ramp = d3.scaleLinear()
-        .domain([min,max])
-        .range([lowColor,highColor]);
+  //get grad rate min and max
+  let max = d3.max(data, function (d, i) {
+    return d[theil]
+  })
+  let min = d3.min(data, function (d, i) {
+    return d[theil]
+  })
 
-    d3.json("https://gist.githubusercontent.com/Bradleykingz/3aa5206b6819a3c38b5d73cb814ed470/raw/a476b9098ba0244718b496697c5b350460d32f99/us-states.json", 
-        function(error, uState) {
-            if (error) throw error;
-            _(uState.features)
-            .keyBy('properties.name')
-            .merge(_.keyBy(data, 'state_id'))
-            .values()
-            .value();
+  //color ramp
+  var ramp = d3.scaleLinear().domain([min, max]).range([lowColor, highColor])
 
-        svg.selectAll('path')
-            .data(uState.features)
-            .enter()
-            .append('path')
-            .attr("d", path)
-            .style('transition', "all 0.2s ease-in-out")
-            .attr('class', 'state')
-            .style('fill','rgb(230, 230, 230)')
+  d3.json(
+    "https://gist.githubusercontent.com/Bradleykingz/3aa5206b6819a3c38b5d73cb814ed470/raw/a476b9098ba0244718b496697c5b350460d32f99/us-states.json",
+    function (error, uState) {
+      if (error) throw error
+      _(uState.features)
+        .keyBy("properties.name")
+        .merge(_.keyBy(data, "state_id"))
+        .values()
+        .value()
+
+      svg
+        .selectAll("path")
+        .data(uState.features)
+        .enter()
+        .append("path")
+        .attr("d", path)
+        .style("transition", "all 0.2s ease-in-out")
+        .attr("class", "state")
+        .style("fill", "rgb(230, 230, 230)")
 
         //adding hover interactions
         .on("mousemove", function (d) {
@@ -85,119 +87,118 @@ d3.csv("bystate_fromcz_rounded.csv", function(data) {
         })
 
         .on("mouseout", function (d, i) {
-            d3.selectAll(".state")
-                .transition()
-                .duration(100)
-                .style("opacity", 1)
-            d3.select(this).style("fill", function (d) {
-                return ramp(d[theil]);
-            });
-            tooltip.transition()
-                .duration(200)
-                .style("opacity", 0)
-        });
-        
+          d3.selectAll(".state").transition().duration(100).style("opacity", 1)
+          d3.select(this).style("fill", function (d) {
+            return ramp(d[theil])
+          })
+          tooltip.transition().duration(200).style("opacity", 0)
+        })
 
-        //get the correct porportion for population size
-        var radius = d3.scaleSqrt()
-            .domain([0, 1e6])
-            .range([0, 6]);
+      //get the correct porportion for population size
+      var radius = d3.scaleSqrt().domain([0, 1e6]).range([0, 6])
 
-        //adding bubble
-        svg.append("g")
-            .attr("class", "bubble")
-            .selectAll("circle")
-            .data(uState.features)
-            .enter().append("circle")
-            .attr("id","bubblemap")
-            .attr("transform", function(d) { return "translate(" + path.centroid(d) + ")"; })
+      //adding bubble
+      svg
+        .append("g")
+        .attr("class", "bubble")
+        .selectAll("circle")
+        .data(uState.features)
+        .enter()
+        .append("circle")
+        .attr("id", "bubblemap")
+        .attr("transform", function (d) {
+          return "translate(" + path.centroid(d) + ")"
+        })
 
-        document.getElementById("theil").onclick = function(){
-            console.log('show Choropleth');
-            d3.selectAll('path')
-            .style("fill", function(d) { 
-                return ramp(d[theil]);
-            })
-        }
-        
-        document.getElementById("state_pop").onclick = function(){
-            console.log('show bubble map');
-            d3.selectAll("#bubblemap")
-            .attr("fill","rgba(0, 0, 0, 0.4)")
-            .attr("stroke","rgba(0,0,0,0.2)")
-            .attr("stroke-width","0.5px")
-            .attr("r", function (d) {
-                    return radius(d.pop2000);
-                });
-        }
+      document.getElementById("theil").onclick = function () {
+        console.log("show Choropleth")
+        d3.selectAll("path").style("fill", function (d) {
+          return ramp(d[theil])
+        })
+      }
 
-        //state abbr
-        // svg.selectAll("text")
-        //     .data(uState.features)
-        //     .enter()
-        //     .append("svg:text")
-        //     .text(function(d){
-        //         return d.state_id;
-        //     })
-        //     .attr("x", function(d){
-        //         return path.centroid(d)[0];
-        //     })
-        //     .attr("y", function(d){
-        //         return  path.centroid(d)[1];
-        //     })
-        //     .attr("text-anchor","middle")
-        //     .attr('font-size','6pt')
-        //     .attr('color','darkgray')
+      document.getElementById("state_pop").onclick = function () {
+        console.log("show bubble map")
+        d3.selectAll("#bubblemap")
+          .attr("fill", "rgba(0, 0, 0, 0.4)")
+          .attr("stroke", "rgba(0,0,0,0.2)")
+          .attr("stroke-width", "0.5px")
+          .attr("r", function (d) {
+            return radius(d.pop2000)
+          })
+      }
 
-        //legend
-        var w = 100, h = 480;
-        var key = d3.select("#chart")
-            .append("svg")
-            .attr("width", w)
-            .attr("height", h)
-            .attr("class", "legend");
+      //state abbr
+      // svg.selectAll("text")
+      //     .data(uState.features)
+      //     .enter()
+      //     .append("svg:text")
+      //     .text(function(d){
+      //         return d.state_id;
+      //     })
+      //     .attr("x", function(d){
+      //         return path.centroid(d)[0];
+      //     })
+      //     .attr("y", function(d){
+      //         return  path.centroid(d)[1];
+      //     })
+      //     .attr("text-anchor","middle")
+      //     .attr('font-size','6pt')
+      //     .attr('color','darkgray')
 
-        var legend = key.append("defs")
-            .append("svg:linearGradient")
-            .attr("id", "gradient")
-            .attr("x1", "100%")
-            .attr("y1", "0%")
-            .attr("x2", "100%")
-            .attr("y2", "100%")
-            .attr("spreadMethod", "pad");
+      //legend
+      var w = 100,
+        h = 480
+      var key = d3
+        .select("#chart")
+        .append("svg")
+        .attr("width", w)
+        .attr("height", h)
+        .attr("class", "legend")
 
-        legend.append("stop")
-            .attr("offset", "0%")
-            .attr("stop-color", highColor)
-            .attr("stop-opacity", 1);
-            
-        legend.append("stop")
-            .attr("offset", "100%")
-            .attr("stop-color", lowColor)
-            .attr("stop-opacity", 1);
+      var legend = key
+        .append("defs")
+        .append("svg:linearGradient")
+        .attr("id", "gradient")
+        .attr("x1", "100%")
+        .attr("y1", "0%")
+        .attr("x2", "100%")
+        .attr("y2", "100%")
+        .attr("spreadMethod", "pad")
 
-        key.append("rect")
-            .attr("width", w - 80)
-            .attr("height", h+20)
-            .style("fill", "url(#gradient)")
-            .attr("transform", "translate(0,10)");
+      legend
+        .append("stop")
+        .attr("offset", "0%")
+        .attr("stop-color", highColor)
+        .attr("stop-opacity", 1)
 
-        var y = d3.scaleLinear()
-            .range([h, 0])
-            .domain([min, max]);
+      legend
+        .append("stop")
+        .attr("offset", "100%")
+        .attr("stop-color", lowColor)
+        .attr("stop-opacity", 1)
 
-        var yAxis = d3.axisRight(y);
+      key
+        .append("rect")
+        .attr("width", w - 80)
+        .attr("height", h + 20)
+        .style("fill", "url(#gradient)")
+        .attr("transform", "translate(0,10)")
 
-        key.append("g")
-            .attr("class", "y axis")
-            .attr("transform", "translate(41,10)")
-            .call(yAxis)
-        });
-    });
+      var y = d3.scaleLinear().range([h, 0]).domain([min, max])
 
+      var yAxis = d3.axisRight(y)
 
+      key
+        .append("g")
+        .attr("class", "y axis")
+        .attr("transform", "translate(41,10)")
+        .call(yAxis)
+    }
+  )
+})
 
-// //line chart 
+// //line chart
 // //code from https://www.d3-graph-gallery.com/graph/line_basic.html
 //     // set the dimensions and margins of the graph
 var margin = { top: 30, right: 30, bottom: 30, left: 60 },
@@ -281,24 +282,32 @@ d3.csv("gender_nat.csv", function (data) {
 
   // Add the scatterplot
   Lsvg.selectAll("dot")
-  .data(data)
-  .enter()
-  .append("circle")
-  .style("fill", "pink")
-  .attr("r", 3.5)
-  .attr("cx", function(d) { return x(d.par_pctile); })
-  .attr("cy", function(d) { return y(d.w2wages_30_f); });
+    .data(data)
+    .enter()
+    .append("circle")
+    .style("fill", "pink")
+    .attr("r", 3.5)
+    .attr("cx", function (d) {
+      return x(d.par_pctile)
+    })
+    .attr("cy", function (d) {
+      return y(d.w2wages_30_f)
+    })
 
   Lsvg.selectAll("dot")
-  .data(data)
-  .enter()
-  .append("circle")
-  .style("fill", "blue")
-  .attr("r", 3.5)
-  .attr("cx", function(d) { return x(d.par_pctile); })
-  .attr("cy", function(d) { return y(d.w2wages_30_m); });
+    .data(data)
+    .enter()
+    .append("circle")
+    .style("fill", "blue")
+    .attr("r", 3.5)
+    .attr("cx", function (d) {
+      return x(d.par_pctile)
+    })
+    .attr("cy", function (d) {
+      return y(d.w2wages_30_m)
+    })
 
-//x axis label
+  //x axis label
   Lsvg.append("text")
     .attr("class", "x label")
     .attr("text-anchor", "end")
@@ -320,18 +329,30 @@ d3.csv("gender_nat.csv", function (data) {
   Lsvg.append("text")
     .attr("class", "x label")
     .attr("text-anchor", "end")
-    .attr("y", Lheight-560)
-    .attr("x", Lwidth -190)
+    .attr("y", Lheight - 560)
+    .attr("x", Lwidth - 190)
     .text(
       "Average Wages of 30 year olds from Varying Parent Income Percentiles"
     )
 })
 
+// ROB ADDING CIRCLES ----------------------------------------
 
+d3.csv("bystate_fromcz_rounded.csv", function (data) {
+  //   console.log(data[32].state_id)
+  //   console.log(data[32].gini)
+
+  // NY name and GINI
+  var state1_name = data[32].state_id
+  var state1_gini = [data[32].gini]
 
   // debug
-  console.log(state2_name)
-  console.log(state2_gini)
+  console.log(state1_name)
+  console.log(state1_gini)
+
+  // FL name and GINI
+  var state2_name = data[9].state_id
+  var state2_gini = data[9].gini
 
   // temp radii for testing (still need math to go from raw GINI to calculated size of circle shown)
   var test_radius = [30, 45]
@@ -356,7 +377,7 @@ d3.csv("gender_nat.csv", function (data) {
     .attr("width", 200)
     .attr("height", 200)
 
-  // add circle to that SVG (as many as there are items in the data array)
+  // add circle 1 to that SVG (as many as there are items in the data array)
   svgSelection
     // .selectAll("circle")
     // .data(test_radius)
@@ -367,6 +388,7 @@ d3.csv("gender_nat.csv", function (data) {
     .attr("r", scale_circle(state1_gini))
     .style("fill", "steelblue")
 
+  // add circle 2 to that SVG (as many as there are items in the data array)
   svgSelection
     // .selectAll("circle")
     // .data(test_radius)
